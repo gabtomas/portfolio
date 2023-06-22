ee04c8c7b511438cac27a4d30fd8a0958d978cc2
gabtomas <46448735+gabtomas@users.noreply.github.com>
2023-06-22 15:07:54 +0200

    fix: photos

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,18 +1,18 @@
 export default [
     {
         title: "MERN Applicationae",
-        imgUrl: "./public/assets/mern_app.png",
+        imgUrl: "./assets/mern_app.png",
         stack: ["MongoDB", "Express", "React", "JWT", "Node"],
         link: "https://mern-users-goals.onrender.com",
     },
     {
         title: "DHL Pickup Scheduler",
-        imgUrl: "./public/assets/dhl.png",
+        imgUrl: "./assets/dhl.png",
         stack: ["Playwright", "Javascript", "Node"],
     },
     {
         title: "Finding hidden API endpoints",
-        imgUrl: "./public/assets/charles.png",
+        imgUrl: "./assets/charles.png",
         stack: ["Charles Proxy", "Api", "Node"],
     },
 ];
